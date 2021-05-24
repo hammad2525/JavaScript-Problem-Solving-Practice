46fcea7ce5c094dde8cbc3f4bb5423a9c8c6e21f
Hammad Khan <hammad.khan5980@gmail.com>
2021-05-24 18:33:06 +0500

    implementation of logical operators

---
+++ Logical=operators/app.js
@@ -0,0 +1,16 @@
+function check(){
+let passingMarks = 30;
+let myMarks =25;
+
+if( myMarks< passingMarks){
+
+    alert("i am fail");
+}
+else if( myMarks > passingMarks){
+
+    alert("i am pass");
+}
+else{
+
+    alert("i am under consideration");
+}}
