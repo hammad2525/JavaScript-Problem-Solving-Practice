dc15ca51ea11bcc0be5e328b2de5e03531e8e40d
Hammad Khan <hammad.khan5980@gmail.com>
2021-05-24 16:31:44 +0500

    implementation of external js button

---
+++ Adding scripts/ExternalJsButton/app.js
@@ -0,0 +1,15 @@
+    // this will show alert when we click on red button
+
+function red(){
+    alert('this red button');
+}
+
+    // this will show alert when we click on green button
+function green(){
+    alert('this is green button');
+}
+
+    // this will show alert when we click on blue button
+function blue(){
+    alert('this is blue button');
+}
