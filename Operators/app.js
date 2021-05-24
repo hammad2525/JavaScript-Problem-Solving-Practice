f19580481b500e2421d1b24469c9325ef3123b2a
Hammad Khan <hammad.khan5980@gmail.com>
2021-05-24 18:19:42 +0500

    arithmetic operation

---
+++ Operators/app.js
@@ -0,0 +1,24 @@
+
+let x=5, y=10, z=15;
+
+let c= x+y;
+console.log(c);
+
+let d= y-x;
+console.log(d);
+
+let e = (x * y);
+console.log(e);
+
+let f = y/x;
+console.log(f);
+
+let g= x % 2;
+console.log(g);
+
+ let r =  x++;
+ console.log(r);
+
+ 
+ x--;
+
