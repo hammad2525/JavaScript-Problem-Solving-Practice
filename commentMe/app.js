6bcace4e88e8e92433b1b19756e725935ca35aa3
Hammad Khan <hammad.khan5980@gmail.com>
2021-05-31 22:48:29 +0500

    sum of two varialbes usin function

---
+++ commentMe/app.js
@@ -0,0 +1,12 @@
+// function add() {
+
+//     let firstNum = parseInt( document.querySelector('#mynum').value);
+
+//     let secondNum = parseInt( document.querySelector('#younum').value);
+
+//     let sum = firstNum + secondNum ; 
+
+//     console.log(sum);
+    
+// }
+
