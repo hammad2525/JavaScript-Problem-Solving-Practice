aaac1311cd59129d7e4732a03be4f5232f524309
Hammad Khan <hammad.khan5980@gmail.com>
2021-05-31 22:35:33 +0500

    checking datatypes of different variables

---
+++ checkIt/app.js
@@ -0,0 +1,29 @@
+
+
+    let a = 8 ;
+    console.log(a);
+    // typeof(a)
+    // "number"
+    
+
+    let b = 8.76;
+    console.log(b);
+
+    let isMale = true;
+    console.log(isMale);
+    // typeof(isMale)
+    // "boolean"
+
+    let name = "hammad";
+    console.log(name);
+    // typeof(name)
+    // "string"
+
+    let myNmae = khan;
+    console.log(myNmae);
+    // typeof(myNmae);
+    // myNmae is not defined
+    
+    let circle = "null";
+    console.log(circle);
+    // myNmae is not defined
