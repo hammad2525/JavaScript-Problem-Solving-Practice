f1b6ab33065f2a0acda2366f0c5275a487e1a65c
Hammad Khan <hammad.khan5980@gmail.com>
2021-06-04 15:15:17 +0500

    declaration of same variable in two differnt functions

---
+++ Variables/variableName/app.js
@@ -0,0 +1,25 @@
+// function declarevariable() {
+
+//     var name = "hammad";
+
+//     console.log(name);
+
+// }
+
+// function myname() {
+
+//     var name = "sad";
+
+//     console.log(name);
+
+// }
+
+//Create a global variable and try using that variable in another function and see if you're able to access that variable
+
+let name = "asad";
+
+function printName() {
+
+    console.log(name);
+    
+}
---
+++ operator/ComparisonOperators/app.js
@@ -0,0 +1,44 @@
+ 
+//  let a = 4;
+//  let b = 5;
+
+//  let comp = 4 <=5 ; 
+
+//  console.log(comp);
+
+ //list of comparison operators
+
+//  ==	equal to	x == 8	false	
+// x == 5	true	
+// x == "5"	true	
+// ===	equal value and equal type	x === 5	true	
+// x === "5"	false	
+// !=	not equal	x != 8	true	
+// !==	not equal value or not equal type	x !== 5	false	
+// x !== "5"	true	
+// x !== 8	true	
+// >	greater than	x > 8	false	
+// <	less than	x < 8	true	
+// >=	greater than or equal to	x >= 8	false	
+// <=	less than or equal to
+
+
+//  let x = 3;
+
+// console.log(x);
+
+// let y = x+1;
+//  console.log(y);
+
+// let z = x+= 3;
+//  console.log(z);
+
+let isSnow = false;
+let isRain = false;
+let isSpeed = 18 < 20 ;
+
+if(isRain || isSnow || isSpeed){
+
+    alert("rocket is launched");
+}
+
