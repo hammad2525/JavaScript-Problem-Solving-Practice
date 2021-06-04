f77cb2ccbe5f89b2fba5860936c4b827a9e25adb
Hammad Khan <hammad.khan5980@gmail.com>
2021-06-04 15:52:25 +0500

    performed arithmetic operations on two variables

---
+++ Operators/Arithmetic operators/app.js
@@ -0,0 +1,29 @@
+function add() {
+
+    let a=5,b=7;
+
+    let sum = a+b;
+
+    alert("sum of two variables is" +sum);
+    
+}
+
+function multiply() {
+
+    let a=5,b=7;
+
+    let multiply = a*b;
+
+    alert("sum of two variables is" +multiply);
+    
+}
+
+function divide() {
+
+    let a=35,b=7;
+
+    let divide = a/b;
+
+    alert("sum of two variables is" +divide);
+    
+}
