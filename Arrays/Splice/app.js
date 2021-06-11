2110918bb17f76ab4841d09d29496670218b47c5
Hammad Khan <hammad.khan5980@gmail.com>
2021-06-11 13:15:29 +0500

    implementation of splice method on array

--- Arrays/Splice/app.js
+++ Arrays/Splice/app.js
@@ -1,5 +1,5 @@
-let games = ["cricket","hockey","snoker","swimming","volley-ball"];
+let MyGames = ["cricket","hockey","snoker","swimming","volley-ball"];
 
- games.splice(2,0,"polo","kabadi");
+ MyGames.splice(2,0,"polo","kabadi");
 
-console.log(games);
+console.log(MyGames);
