39224d26806d213d73b8b610ea0fa841a89b4245
Hammad Khan <hammad.khan5980@gmail.com>
2021-06-11 10:52:38 +0500

    printing array without using for loop

---
+++ Arrays/IntroToArrays/app.js
@@ -0,0 +1,7 @@
+let games = ["cricket","hockey","snoker","swimming","kabadi"];
+
+console.log(games[0]);
+console.log(games[1]);
+console.log(games[2]);
+console.log(games[3]);
+console.log(games[4]);
