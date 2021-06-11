92e67595f9783db0dec85ee9f0383bb637f0ddfd
Hammad Khan <hammad.khan5980@gmail.com>
2021-06-11 12:59:20 +0500

    differnt utility methods

---
+++ Arrays/Utility Methods/app.js
@@ -0,0 +1,14 @@
+let games = ["cricket","hockey","snoker","swimming","kabadi"];
+
+games.push('polo');
+
+console.log(games);
+
+games.unshift('polo');
+console.log(games);
+
+games.pop();
+console.log(games);
+
+games.shift();
+console.log(games);
