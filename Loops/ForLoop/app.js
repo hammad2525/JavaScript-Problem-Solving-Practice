b4f28f6432fa2437d164765980cea326943bb61e
Hammad Khan <hammad.khan5980@gmail.com>
2021-06-11 08:46:59 +0500

    created a for loop

---
+++ Loops/ForLoop/app.js
@@ -0,0 +1,13 @@
+// A . create for loop
+
+for( let i = 0 ; i < 50 ; i++ ){
+
+    console.log("Clap!"+i)
+}
+
+// B . create for loop
+
+for ( let i = 0 ; i < 200 ; i++ ) {
+
+    console.log(i);
+}
---
+++ Operators/incrementOperator/app.js
@@ -0,0 +1,7 @@
+let score = 0;
+
+function show(){
+    score += 5;
+
+    document.querySelector('p').innerHTML = score;
+}
