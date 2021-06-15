9688ea0978f8bec226f11af48fadf1bee5512173
Hammad Khan <hammad.khan5980@gmail.com>
2021-06-15 20:59:36 +0500

    implemetnation of number method

---
+++ OBJECTS/JS Utility Methods/NumberMethod/app.js
@@ -0,0 +1,11 @@
+// Use the Number method to convert the given variable age = ‘44’ 
+// and find it’s type before and after the conversion
+
+let age = '44';
+console.log(typeof(age));
+
+let i = parseInt(age);
+console.log(typeof(i));
+
+let f = parseInt(age);
+console.log(typeof(f));
