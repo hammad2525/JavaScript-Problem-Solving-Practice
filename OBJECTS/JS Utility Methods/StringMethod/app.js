ce912b2279c2ebc251219e643a654569eba13e77
Hammad Khan <hammad.khan5980@gmail.com>
2021-06-15 20:52:35 +0500

    implementation of string method

---
+++ OBJECTS/JS Utility Methods/StringMethod/app.js
@@ -0,0 +1,19 @@
+// Use .length property to print length of a string cityName = ‘Tokyo’
+
+let cityName = 'tokyo';
+
+let a = cityName.length;
+
+console.log(a);
+
+
+// Find the index of ‘o’ character in the given string using indexOf method
+
+let b = cityName.indexOf('o');
+console.log(b);
+
+// Use search method to locate ‘o’ in the given string and notice 
+// the difference between indexOf and search methods
+
+let s = cityName.search('o');
+console.log(s);
