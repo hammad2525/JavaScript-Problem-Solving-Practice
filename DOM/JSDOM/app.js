d5fab44ceaa60f297db745bf63ecbb8aea563566
Hammad Khan <hammad.khan5980@gmail.com>
2021-06-19 09:31:18 +0500

    implemetnation of JSDOM

---
+++ DOM/JSDOM/app.js
@@ -0,0 +1,11 @@
+
+let element = document.getElementsByClassName('div-class').innerText =" changed-class";
+
+console.log(element);
+
+document.getElementById('mydiv').style.backgroundColor="red";
+document.getElementById('mydiv').style.color="blue";
+document.getElementById('mydiv').style.border= "2px solid green";
+
+
+
